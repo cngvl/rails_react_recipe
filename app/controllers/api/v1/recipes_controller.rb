87e5018e7eb0d4358a3308abf643b12a0b83d148
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-03-24 15:32:25 +1100

    updaing recipe controller actions and seeding data

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,36 @@
 class Api::V1::RecipesController < ApplicationController
-  def index
-  end
+  before_action :set_recipe, only: %i[show destroy]
 
-  def create
+  def index
+    recipe = Recipe.all.order(created_at: :desc)
+    render json: recipe
   end
 
   def show
+    render json: @recipe
+  end
+
+  def create
+    recipe = Recipe.create!(recipe_params)
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def destroy
+    @recipe&.destroy
+    render json: { message: 'Recipe deleted!' }
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name, :image, :ingredients, :instruction)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
---
+++ app/javascript/components/Recipes.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Recipes = () => {
+  const navigate = useNavigate();
+  const [recipes, setRecipes] = useState([]);
+};
+
+export default Recipes;
